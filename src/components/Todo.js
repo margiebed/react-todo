e9eb4d51cc4b7852b60bee5619bf52b9a6aae17e
Malgorzata Bednarczuk <margie@ais.pl>
2021-12-29 22:35:40 +0100

    add fauture completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,10 +3,21 @@ const Todo = ({ text, todo, todos, setTodos }) => {
   const deleteHandler = () => {
     setTodos(todos.filter((el) => el.id !== todo.id))
   }
+  const completeHandler = () => {
+      setTodos(todos.map((item)=> {
+          if (item.id === todo.id){
+              return {
+                  ...item, completed: !item.completed
+              }
+          }
+          return item
+      }))
+  }
+  
   return (
     <div className="todo">
-      <li className="todo-item">{text}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>{text}</li>
+      <button className="complete-btn" onClick={completeHandler}>
         <i className="fas fa-check"></i>
       </button>
       <button className="trash-btn" onClick={deleteHandler}>
