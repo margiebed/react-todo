77d229a1ceaf82a2d9f50730c875d2bfaf3f5bf0
Malgorzata Bednarczuk <margie@ais.pl>
2021-12-29 21:28:38 +0100

    add button submit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import TodoList from './components/ToDoList'
 
 function App() {
   const [inputText, setInputText] = useState('')
+  const [todos, setTodos] = useState([])
   return (
     <div className="App">
       <header>
         <h1>Gosia TODO list</h1>
       </header>
-      <Form  setInputText={setInputText}/>
+      <Form setInputText={setInputText} todos={todos} setTodos={setTodos} inputText={inputText} />
       <TodoList />
     </div>
   )
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
-const Form = ({setInputText}) => {
+const Form = ({ setInputText, todos, setTodos, inputText }) => {
   const inputTextHandler = (e) => {
     console.log(e)
     setInputText(e.target.value)
   }
+  const submitTodoHandler = (e) => {
+    e.preventDefault()
+    setTodos([...todos, { text: inputText, completed: false, id: Math.random() * 1000 }])
+  }
   return (
     <form>
       <input onChange={inputTextHandler} type="text" className="todo-input" />
-      <button className="todo-button" type="submit">
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
