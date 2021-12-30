e8110de99000c3dc3877848a3f0dd09bc94eeb3b
Malgorzata Bednarczuk <margie@ais.pl>
2021-12-30 14:48:06 +0100

    fixed space

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,19 +1,23 @@
 import React from 'react'
+
 const Todo = ({ text, todo, todos, setTodos }) => {
   const deleteHandler = () => {
     setTodos(todos.filter((el) => el.id !== todo.id))
   }
   const completeHandler = () => {
-      setTodos(todos.map((item)=> {
-          if (item.id === todo.id){
-              return {
-                  ...item, completed: !item.completed
-              }
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
           }
-          return item
-      }))
+        }
+        return item
+      })
+    )
   }
-  
+
   return (
     <div className="todo">
       <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>{text}</li>
